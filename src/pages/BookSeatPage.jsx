2bbf9376d95bd307a8229d0144dd4a37698d936e
Santiago Spina <santiagospina025@gmail.com>
2025-10-22 19:02:12 +0200

    Select en bookSeat con ids

--- src/pages/BookSeatPage.jsx
+++ src/pages/BookSeatPage.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { bookSeat } from "../services/AppService";
+import { bookSeat, findAll } from "../services/AppService";
 
 export const BookSeatPage = () => {
   const [form, setForm] = useState({ userId: "", seatId: "" });
+  const [seatsIds, setSeatsIds] = useState([]);
+  const [usersIds, setUsersIds] = useState([]);
+
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -16,33 +19,81 @@ export const BookSeatPage = () => {
     if (result) navigate("/users");
   };
 
+  const getSeatsIds = async () => {
+    try {
+      const result = await findAll("seats");
+      const availableSeats = result.data.filter((s) => s.status == "AVAILABLE");
+      const availableSeatsIds = availableSeats.map((s) => {
+        return s.id;
+      });
+
+      setSeatsIds(availableSeatsIds);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getUsersIds = async () => {
+    try {
+      const result = await findAll("users");
+      const usersIdsFound = result.data.map((u) => {
+        return u.id;
+      });
+
+      setUsersIds(usersIdsFound);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSeatsIds();
+    getUsersIds();
+  }, []);
+
   return (
     <div className="container mt-5">
       <h2 className="mb-4">Reservar un asiento para un usuario</h2>
 
       <form onSubmit={handleSubmit} className="col-md-6 mx-auto">
         <div className="mb-3">
-          <label className="form-label required">Id del usuario</label>
-          <input
-            type="text"
+          <label htmlFor="userId" className="form-label required">
+            Id del usuario
+          </label>
+          <select
+            id="userId"
             name="userId"
-            className="form-control"
-            value={form.userId}
+            className="form-select"
             onChange={handleChange}
-            required
-          />
+            value={form.userId}
+          >
+            <option value="">Selecciona el id del usuario</option>
+            {usersIds.map((id) => (
+              <option key={id} value={id}>
+                {id}
+              </option>
+            ))}
+          </select>
         </div>
 
         <div className="mb-3">
-          <label className="form-label required">Id del asiento</label>
-          <input
-            type="text"
+          <label htmlFor="seatId" className="form-label required">
+            Id del asiento
+          </label>
+          <select
+            id="seatId"
             name="seatId"
-            className="form-control"
-            value={form.seatId}
+            className="form-select"
             onChange={handleChange}
-            required
-          />
+            value={form.seatId}
+          >
+            <option value="">Selecciona el id del asiento</option>
+            {seatsIds.map((id) => (
+              <option key={id} value={id}>
+                {id}
+              </option>
+            ))}
+          </select>
         </div>
 
         <button type="submit" className="btn btn-success me-3">
