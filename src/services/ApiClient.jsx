2ea8f03accfb68ef55fec8480472dd84462c76d3
SantiagoSpina25 <santiagospina25@gmail.com>
2025-12-12 20:51:26 +0100

    env url

--- src/services/ApiClient.jsx
+++ src/services/ApiClient.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://flights-app-backend.onrender.com/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://flights-app-backend.onrender.com/api";
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
